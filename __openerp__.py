698df4c580bff453dcba73bb8c6f4b1b02ef3a81
morrillo <gustavo.orrillo@gmail.com>
2017-06-28 18:37:34 -0300

    First commit

---
+++ __openerp__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'MercadoPago tupulperia',
+    'category': 'Hidden',
+    'summary': 'Payment Acquirer: MercadoPago tupulperia',
+    'version': '1.0',
+    'description': """MercadoPago tupulperia""",
+    'author': 'Moldeo Interactive - www.moldeo.coop',
+    'depends': ['payment_mercadopago'],
+    'data': [
+        'mercadopago.xml'
+    ],
+    'installable': True,
+}
